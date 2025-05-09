c8d1b15f72c7ef031ad77a831dc30d757ed7c5e6
gkawanami09 <guilherme.kawanami@gmail.com>
2025-05-09 14:48:40 -0300

    Finally back-end
    correção de bugs

--- main.py
+++ main.py
@@ -16,10 +16,7 @@ def catalogo():
 
 @app.route('/emprestimos')
 def emprestimos():
-    livros_emprestados = []
-    for livro in livros:
-        if livro['emprestado'] == True:
-            livros_emprestados.append(livro)
+    livros_emprestados = [l for l in livros if l['emprestado']]
     return render_template('emprestimos.html', livros=livros_emprestados)
 
 @app.route('/adicionar', methods=['GET', 'POST'])
@@ -42,10 +39,10 @@ def adicionar():
                 'data_devolucao': None
             }
             livros.append(novo_livro)
-            flash(f'Livro {titulo} adicionado com sucesso!')
+            flash(f"Livro {titulo} adicionado com sucesso!")
             return redirect('/')
         except Exception as e:
-            return render_template('erro.html', mensagem='Erro ao adicionar livro: ' + str(e))
+            return render_template('erro.html', mensagem=f'Erro ao adicionar livro: {e}')
     return render_template('form.html', titulo='Adicionar Livro', livro=None)
 
 @app.route('/editar/<int:codigo>', methods=['GET', 'POST'])
@@ -59,20 +56,26 @@ def editar(codigo):
             if ano < 0 or ano > 2025:
                 raise ValueError('Ano inválido. Informe um ano entre 0 e 2025.')
             livro['ano'] = ano
-            flash(f'Livro {livro['titulo']} editado com sucesso!')
+            flash(f"Livro {livro['titulo']} editado com sucesso!")
             return redirect('/')
         return render_template('form.html', titulo='Editar Livro', livro=livro)
     except Exception as e:
-        return render_template('erro.html', mensagem='Erro ao editar livro: ' + str(e))
+        return render_template('erro.html', mensagem=f'Erro ao editar livro: {e}')
 
 @app.route('/excluir/<int:codigo>')
 def excluir(codigo):
     try:
         titulo = livros[codigo]['titulo']
         livros.pop(codigo)
-        flash(f'Livro {titulo} excluído com sucesso!')
+        for ADE in range(len(livros)):
+            livros[ADE]['codigo'] = ADE
+
+        #for ADE, livro in enumerate(livros): # enumerate serve para substituir o ADE=0 ---- ADE+=1
+         #   livro['codigo'] = ADE
+
+        flash(f"Livro {titulo} excluído com sucesso!")
     except Exception as e:
-        return render_template('erro.html', mensagem='Erro ao excluir livro: ' + str(e))
+        return render_template('erro.html', mensagem=f'Erro ao excluir livro: {e}')
     return redirect('/')
 
 @app.route('/emprestar/<int:codigo>')
@@ -86,7 +89,7 @@ def emprestar(codigo):
         data_str = livro['data_devolucao'].strftime('%d/%m/%Y')
         flash(f"Livro {livro['titulo']} emprestado até {data_str}.")
     except Exception as e:
-        return render_template('erro.html', mensagem='Erro ao emprestar livro: ' + str(e))
+        return render_template('erro.html', mensagem=f'Erro ao emprestar livro: {e}')
     return redirect('/')
 
 @app.route('/devolver/<int:codigo>')
@@ -95,16 +98,18 @@ def devolver(codigo):
         livro = livros[codigo]
         hoje = datetime(2025, 5, 1)
         atraso = (hoje - livro['data_devolucao']).days
+        if hoje < livro['data_emprestimo']:
+            return render_template('erro.html', mensagem='Erro, não aceitamos devolução de um viajante do tempo!')
         if atraso > 0:
             multa = 10 + (10 * 0.01 * atraso)
-            flash('Livro devolvido com ' + str(atraso) + ' dias de atraso. Multa: R$ ' + '{:.2f}'.format(multa))
+            flash(f"Livro devolvido com {atraso} dias de atraso. Multa: R$ {multa:.2f}")
         else:
-            flash(f'Livro {livro['titulo']} devolvido sem atraso.')
+            flash(f"Livro {livro['titulo']} devolvido sem atraso.")
         livro['emprestado'] = False
         livro['data_emprestimo'] = None
         livro['data_devolucao'] = None
     except Exception as e:
-        return render_template('erro.html', mensagem='Erro ao devolver livro: ' + str(e))
+        return render_template('erro.html', mensagem=f'Erro ao devolver livro: {e}')
     return redirect('/')
 
 if __name__ == '__main__':
